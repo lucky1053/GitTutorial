9686c200f3690031f25dccc3b6f47b52747b7067
lucky1053 <jain.lucky1053@gmail.com>
2022-06-29 04:26:40 -0700

    Added SYSO line

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,6 @@
 public class FirstTest {
     public static void main(String[] args) {
         System.out.println("First Test Project");
+        System.out.println("First changed line");
     }
 }
